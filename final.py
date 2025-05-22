6da1a756bd207a5585bbe77bd293a2d25f612853
Lamed(라메드) <53391835+Lamed-D@users.noreply.github.com>
2025-05-23 00:36:53 +0900

    Add files via upload

--- final.py
+++ final.py
@@ -7,8 +7,11 @@ def for_Koch(dist, depth):
         t.forward(dist)
         return
 
+    #그려지는 모든 직선의 길이는 동일함
+    #수식으로는 dist / 3의depth제곱
     length = dist / (3 ** depth)
-    max_count = 4 ** (depth - 1) + 1 if depth > 0 else 1
+    #max_count는 직선의 개수를 의미
+    max_count = 4 ** (depth - 1) + 1
     flags = [0] * (depth - 1)  # 플래그 리스트: depth-1개 플래그 관리
 
     for i in range(1, max_count):
@@ -46,6 +49,7 @@ def for_Koch(dist, depth):
 t.penup()
 t.goto(-300, 0)
 t.pendown()
+##진짜 그리는 함수
 t.speed(0)
 for_Koch(500, 5)  # depth=6 포함 정상 동작 확인
 t.done()
